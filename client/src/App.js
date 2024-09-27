78d2174c120f9601212c45b7587e563a101f2dd6
Colton <cgl657s@missouristate.edu>
2024-09-27 13:56:41 -0500

    MySQL added

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,31 @@
 import React, {useState, useEffect} from 'react'
 
-function App() {
+import axios from 'axios';
 
-  const [data, setData] = useState([{}])
+const DataComponent = () => {
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch("/members").then(
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-        console.log(data)
-      }
-    )
-  }, [])
+    // Fetch data from Flask backend
+    axios.get('http://localhost:5000/get_data')
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error!', error);
+      });
+  }, []);
 
   return (
     <div>
-
-        {(typeof data.members == 'undefined') ? (
-          <p>Loading...</p>
-        ) : (
-          data.members.map((member, i) => (
-            <p key={i}>{member}</p>
-          ))
-        )}
-
+      <h1>Data from MySQL</h1>
+      <ul>
+        {data.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default DataComponent;
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,12 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS  
+import mysql.connector
 
 app = Flask(__name__)
+CORS(app)
 
-#Members API Route
+# Configure MySQL connection
+db = mysql.connector.connect(
+    host="localhost",       # or your MySQL server address
+    user="root",    # MySQL username
+    password="CSC450-UniVentures",# MySQL password
+    database="users" # Database name
+)
 
-@app.route("/members")
-def members():
-    return {"members": ["Member1", "Member2", "Member3"]}
+cursor = db.cursor()
+
+@app.route('/get_data', methods=['GET'])
+def get_data():
+    cursor.execute("SELECT * FROM user")
+    result = cursor.fetchall()
+    return jsonify(result)
 
 if __name__ == "__main__":
     app.run(debug=True)

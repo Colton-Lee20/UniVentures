ff664b9120711acd18b83705a4e2b2cab21564ed
Colton <cgl657s@missouristate.edu>
2024-09-30 00:18:03 -0500

    Navigation Bar Partially Complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,55 @@
-import React, {useState, useEffect} from 'react'
-
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import "./css/input.css";
+
+function App() {
+  return (
+    <div className="bg-[#ffffff] w-screen h-screen overflow-hidden">
+      {/* navbar */}
+      <div className='w-screen py-6 px-5 lg:px-10 bg-[#252525] flex justify-between text-neutral-300'>
+        <span className='text-lg font-semibold'>
+          U V
+        </span>
+
+        <ul className='hidden md:flex items-center space-x-5'>
+          <li>
+            About
+          </li>
+          <li>
+            Contact
+          </li>
+          <li>
+            Account
+          </li>
+        </ul>
+
+        {/*Hamburger Menu*/}
+        <button className='space-y-1 group md:hidden'>
+          <div className='w-6 h-1 bg-white'></div>
+          <div className='w-6 h-1 bg-white'></div>
+          <div className='w-6 h-1 bg-white'></div>
+
+          {/* Menu */}
+          <ul className='bg-[252525] w-screen pb-10 absolute -top-full group-focus:top-0 right-0 duration-150 flex flex-col space-y-3 justify-end'>
+            <button className='px-10 py-8 relative ml-auto'></button>
+            <li className='flex justify-center w-full py-4 bg-[#252525] hover:bg-[#D3D3D3]'>
+              About
+            </li>
+            <li className='flex justify-center w-full py-4 bg-[#252525] hover:bg-[#D3D3D3]'>
+              Contact
+            </li>
+            <li className='flex justify-center w-full py-4 bg-[#252525] hover:bg-[#D3D3D3]'>
+              Account
+            </li>
+          </ul>
+        </button>
+
+      </div>
+      
+
+    </div>
+  );
+}
 
 const DataComponent = () => {
   const [data, setData] = useState([]);
@@ -28,4 +77,4 @@ const DataComponent = () => {
   );
 };
 
-export default DataComponent;
+export default App;

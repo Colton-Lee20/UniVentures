7d950c64ea6791b1551cdb862bc3187bc31ca5b8
Colton <cgl657s@missouristate.edu>
2024-10-16 19:37:40 -0500

    Made about and contact page

--- client/src/About.js
+++ client/src/About.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './css/input.css';
 import Banner from './Banner';
+import background from './Images/About-Background.jpg'
 
 function About() {
     return (
-    <div className="bg-[#ffffff] dark:bg-gray-800 w-screen h-screen overflow-hidden">
+    <div class='w-screen h-screen bg-cover bg-center' style={{ backgroundImage: `url(${background})` }}>
         <Banner/>
-        <h2>About Page</h2>
+            <div className='flex flex-col items-center mt-40 mx-40 px-10 py-10 rounded-2xl  bg-[#101c26]'>
+                <div className='text-white font-serif text-5xl mb-4'>UniVentures</div>
+                <h2 className='text-white text-center'>
+                Univentures is your go-to web app for discovering exciting things to do around 
+                your college campus! Whether you're looking for a new place to eat, explore, or 
+                socialize, Univentures helps you find nearby adventures tailored to your university 
+                experience. Users can discover recommended spots added by others or browse locations 
+                via Google’s Places API. You can rate, review, and even create your own adventures—after 
+                verifying your university email. For those feeling spontaneous, the “Surprise Me!” feature 
+                picks a random adventure for you. No account? No problem! You can still explore, but creating 
+                and reviewing adventures requires one. Plan your next adventure with Univentures!
+                </h2>
+            </div>
     </div>
     );
 }
--- client/src/Contact.js
+++ client/src/Contact.js
@@ -6,8 +6,71 @@ function Contact() {
     return (
     <div className="bg-[#ffffff] dark:bg-gray-800 w-screen h-screen overflow-hidden">
         <Banner/>
-        <div>
-            Contact page!
+        <div className='bg-[#101c26]'>
+            <section className='py-2'>
+                <div className='max-w-6xl mx-auto p-4 md:p-16 xl:p-20'>
+                    <div className='lg:w-2/3 space-y-5 text-center mx-auto'>
+                        <h1 className='text-white uppercase tracking-wides font-medium text-4xl'>
+                            Get in touch
+                        </h1>
+                        <div className='h-0.5 bg-red-500 w-14 mx-auto'></div>
+                        <p className='text-gray-300 text-base leading-6'>
+                            We'd love to hear from you!
+                        </p>
+                    </div>
+                    <div className='grid grid-cols-1 gap-6 mt-16'>
+                        <div className='lg:col-span-2'>
+                            <form action=''>
+                                <div className='space-y-6'>
+                                    <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+                                        <label for='name' className='sr-only'>Name</label>
+                                        <input
+                                        type='text'
+                                        name='name'
+                                        id='name'
+                                        className='border text-gray-900 text-sm rounded focus:ring-0 focus:border-gray-400 block w-full p-3 bg-zinc-700/20 border-zinc-700/50 placeholder:text-gray-300/50 text-gray-300/50'
+                                        placeholder='Enter your name'
+                                        />
+                                        <label for='email' className='sr-only'>Email</label>
+                                        <input
+                                        type='email'
+                                        name='email'
+                                        id='email'
+                                        className='border text-gray-900 text-sm rounded focus:ring-0 focus:border-gray-400 block w-full p-3 bg-zinc-700/20 border-zinc-700/50 placeholder:text-gray-300/50 text-gray-300/50'
+                                        placeholder='Enter your email'
+                                        />
+                                    </div>
+                                    <label for='subject' className='sr-only'>Subject</label>
+                                        <input
+                                        type='text'
+                                        name='subject'
+                                        id='subject'
+                                        className='border text-gray-900 text-sm rounded focus:ring-0 focus:border-gray-400 block w-full p-3 bg-zinc-700/20 border-zinc-700/50 placeholder:text-gray-300/50 text-gray-300/50'
+                                        placeholder='Enter your subject'
+                                        />
+                                        <label for='comments' className='sr-only'>Message</label>
+                                        <textarea
+                                        name='comments'
+                                        id='comments'
+                                        className='border text-gray-900 text-sm rounded focus:ring-0 focus:border-gray-400 block w-full p-3 bg-zinc-700/20 border-zinc-700/50 placeholder:text-gray-300/50 text-gray-300/50'
+                                        placeholder='Enter your Message'
+                                        rows='3'
+                                        ></textarea>
+                                        <div className='text-right'>
+                                            <input
+                                            type='submit'
+                                            id='submit'
+                                            name='send'
+                                            className='inline-block px-15 py-2.5 px-2.5 rounded text-sm cursor-pointer select-none outline-none transition-all duration-500 focus:ring-offset-0 hover:-translate-y-1.5 bg-red-500 text-white'
+                                            value='Send Message'
+                                            />
+                                        </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
     </div>
     );

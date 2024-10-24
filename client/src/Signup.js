9e40ad20d9f05f83008c498a7c43ae7e684f9270
Caleb Luebbering <calube4042@gmail.com>
2024-10-24 16:46:23 -0500

    fix scrollbars, search text

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -32,7 +32,7 @@ function Signup() {
           }
       };
       checkLoginStatus();
-    }); 
+    }, []); 
 
     //ACCOUNT SIGNUP
     const handleSubmit = async (e) => {
@@ -65,9 +65,8 @@ function Signup() {
 
     // COOKIE DOES NOT EXIST - render sign up page
     return (
-    <div className="bg-[#ffffff] dark:bg-gray-800 w-screen min-h-screen overflow-hidden">
-        <Banner/>
-        <div className='bg-[#101c26] min-h-screen'>
+        <div className='bg-[#101c26] min-h-screen overflow-auto'>
+          <Banner/>
             <section className='py-2'>
                 <div className='max-w-lg mx-auto p-4 md:p-16 xl:p-20'>
                     <div className='lg:w-2/3 space-y-5 text-center mx-auto'>
@@ -134,7 +133,6 @@ function Signup() {
                 </div>
             </section>
         </div>
-    </div>
     );
 }
 
